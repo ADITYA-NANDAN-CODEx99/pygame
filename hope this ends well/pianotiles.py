8941a2b29faa1820e6b35e984bcda0a27e80ee2f
Aditya Nandan <61308761+ADITYA-NANDAN-CODEx99@users.noreply.github.com>
2020-11-08 19:26:05 +0530

    another tragedy uploaded

--- hope this ends well/pianotiles.py
+++ hope this ends well/pianotiles.py
@@ -4,27 +4,52 @@ pygame.init()
 
 WW, WH = 400, 600
 WINDOW = pygame.display.set_mode((WW, WH))
-pygame.display.set_caption('IDK THE NAME YET!')
+pygame.display.set_caption('Piano Tiles')
 clock = pygame.time.Clock()
 
 
-def reset_tiles(t_template, t_list):
-    t_template = tile.copy()
-    t_template.x = i + 7.5
-    t_list.append(t_template)
+def new_tiles_1b(t_template):
+    empty = random.choice([0, 1, 2, 3, 4])
+    t_list = []
+    for i, n in zip(range(0, WW+1, WW//5), range(5)):
+        if n != empty:
+            t = t_template.copy()
+            t.x = i + 7.5
+            t_list.append(t)
+    return t_list
+
+def new_tiles_4b(t_template):
+    empty = random.choice([0, 1, 2, 3])
+    t_list = []
+    for i, n in zip(range(0, WW+1, WW//4), range(4)):
+        if n == empty:
+            t = t_template.copy()
+            t.x = i + 7.5
+            t_list.append(t)
+    return t_list
+
+def move_tiles(t_list, t_template, vel):
+    
+    for t in t_list:
+            pygame.draw.rect(WINDOW, (255, 255, 255), t)
+
+            t.y += vel
+
+            if t.top > WH:
+                t_list = new_tiles_4b(t_template)
+                if vel < 10:
+                    vel += 1
+                break
 
 def main():
     # some main vars
-    tiles = []
     tw, th = WW/6, 100
     vy = 5
-    tile = pygame.Rect(0, 0, tw, th)
-    pos = []
-    for i in range(0, WW-WW//5, WW//5):
-        pos.append(i)
-        
-
-    empty = random.choice([0, 1, 2, 3, 4])
+    tile1 = pygame.Rect(0, -th, tw, th)
+    tile2 = pygame.Rect(0, -th*3, tw, th)
+    
+    tiles1 = new_tiles_4b(tile1)
+    tiles2 = new_tiles_4b(tile2)
 
     running = True
     while running:
@@ -36,14 +61,8 @@ def main():
             if e.type == pygame.QUIT:
                 running = False
 
-
-        for t in tiles:
-            pygame.draw.rect(WINDOW, (255, 255, 255), t)
-            if tiles.index(t) != empty:
-                t.y += vy
-            if t.top > WH:
-                t.bottom = 0
-                empty = random.choice([0, 1, 2, 3, 4])
+        move_tiles(tiles1, tile1, vy)
+        move_tiles(tiles2, tile2, vy)
 
         pygame.display.update()
 
